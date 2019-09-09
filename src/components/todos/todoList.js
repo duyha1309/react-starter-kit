c829f100c5170781346596713905247c723f5af8
duyha1309 <ha.le@dinovative.com>
2019-09-09 18:14:20 +0700

    update

--- src/components/todos/todoList.js
+++ src/components/todos/todoList.js
@@ -1,1 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+import Todo from './todo';
+
+const TodoList = ({ todos, toggleTodo}) => {
+
+  return (
+
+  )
+};
+
+TodoList.propTypes = {
+  todos: PropTypes.arrayOf(
+    PropTypes.shape({
+
+    })
+  ),
+  toggleTodo: PropTypes.func.isRequired
+}
+
+export default TodoList;
